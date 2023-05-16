11d13079121e9afd08303b260cf59a3e0b37bed1
charann90 <ccharancs543@gmail.com>
2023-05-16 11:32:09 +0530

    code commit

--- grade_app/views.py
+++ grade_app/views.py
@@ -7,19 +7,27 @@ from grade_app.util import format_serializer_data
 
 
 def dashboard(request):
-    rows_qs=Grades.objects.all()
-    rows_s=GradesSerializer(rows_qs,many=True)
-    rows_data=format_serializer_data(rows_s)
+    rows_qs = Grades.objects.filter().all()
+    rows_s = GradesSerializer(rows_qs, many=True)
+    rows_data = format_serializer_data(rows_s.data)
     print('abc')
-    print(rows_data,rows_data[0],rows_data[0]["student_name"])
-    return render(request,'dashboard.html',{'rows':rows_data})
+    print(rows_data, rows_data[0], rows_data[0]["student_name"])
+    return render(request, 'dashboard.html', {'rows': rows_data})
+
+
 def form(request):
-    if request.method == 'POST':
-        print(request)
-        student_name = request.POST['student_name']
-        class_name = request.POST['class_name']
-        grade = request.POST['grade']
-        marks = request.POST['marks']
-        new_entry=Grades(student_name=student_name,class_name=class_name,grade=grade,marks=marks)
-        new_entry.save()
-    return render(request,'form.html')
+    try:
+        if request.method == 'POST':
+            print(request)
+            student_name = request.POST['student_name']
+            class_name = request.POST['class_name']
+            grade = request.POST['grade']
+            marks = request.POST['marks']
+            new_entry = Grades(student_name=student_name, class_name=class_name, grade=grade, marks=marks)
+            new_entry.save()
+            return render(request, 'post_submit.html')
+    except Exception as e:
+        print(e)
+        return render(request,'submit_fail.html')
+
+    return render(request, 'form.html')
